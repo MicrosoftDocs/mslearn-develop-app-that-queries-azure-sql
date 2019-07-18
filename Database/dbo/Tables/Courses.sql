53a268cf7877bb817153fa56910304be0adbadcd
abel <abel.wang@gmail.com>
2019-07-17 17:45:44 -0700

    trying chocolatey

---
+++ Database/dbo/Tables/Courses.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Courses] (
+    [CourseID]   INT          NOT NULL,
+    [CourseName] VARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([CourseID] ASC)
+);
+
---
+++ Database/dbo/Tables/Modules.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Modules] (
+    [ModuleCode]  VARCHAR (5)  NOT NULL,
+    [ModuleTitle] VARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([ModuleCode] ASC)
+);
+
