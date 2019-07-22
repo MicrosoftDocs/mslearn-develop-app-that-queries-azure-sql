864477d41137ed4c3ffff094257fa2a418ba2ab3
abel <abel.wang@gmail.com>
2019-07-22 11:53:06 -0700

    looking at the file system

--- infra/provisionWebDB.ps1
+++ infra/provisionWebDB.ps1
@@ -258,6 +258,21 @@ Write-Output ""
     refreshenv
     Write-Output "done refreshing environment"
 
+    cd C:\
+    ls
+    cd "Program Files"
+    ls
+    cd "Microsoft SQL Server"
+    ls
+    cd "Client SDK"
+    ls
+    cd "ODBC"
+    ls
+    cd "130"
+    ls
+    cd Tool
+    ls
+
     Write-Output "Checking data for Courses..."
     $numRows=$(Invoke-Sqlcmd -ConnectionString "Server=tcp:abellearndbserver1.database.windows.net,1433;Initial Catalog=learndb;Persist Security Info=False;User ID=abel;Password=g83P@BxDXma700000;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;" -Query "SELECT Count(*) FROM Courses")
     if ($numRows.Column1 -eq 0) {
