7df9ddf3d56208b786561ddece16bddf80279358
abel <abel.wang@gmail.com>
2019-07-24 10:50:55 -0700

    changed database to the right version, made modifications to CoursesAndModules.cs

--- Models/CoursesAndModules.cs
+++ Models/CoursesAndModules.cs
@@ -2,8 +2,15 @@
 {
     public class CoursesAndModules
     {
-        // TODO: Define the CourseName, ModuleTitle, and Sequence read-only properties
-        
-        // TODO: Create a constructor that initializes the fields behind the properties
-    }
+        public string CourseName { get; }
+        public string ModuleTitle { get; }
+        public int Sequence { get; }
+
+
+        public CoursesAndModules(string courseName, string moduleTitle, int sequence)
+        {
+            this.CourseName = courseName;
+            this.ModuleTitle = moduleTitle;
+            this.Sequence = sequence;
+        }
 }
