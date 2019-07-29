fc197c61c43b3d983e3b0b3b31fb383281055a70
abel <abel.wang@gmail.com>
2019-07-29 11:58:56 -0700

    removed unused reference to lib

--- WebSite/Startup.cs
+++ WebSite/Startup.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using CoursesWebApp.lib;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
