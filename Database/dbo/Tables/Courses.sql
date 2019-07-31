69d5f7b8cdde6b21827603f4f1e33e17e72b109a
abel <abel.wang@gmail.com>
2019-07-30 18:40:27 -0700

    reverted environment

--- Database/dbo/Tables/Courses.sql
+++ Database/dbo/Tables/Courses.sql
@@ -1,7 +1,6 @@
 ﻿CREATE TABLE [dbo].[Courses] (
     [CourseID]   INT          NOT NULL,
     [CourseName] VARCHAR (50) NOT NULL,
-    [Teacher] VARCHAR(50) NULL, 
     PRIMARY KEY CLUSTERED ([CourseID] ASC)
 );
 
