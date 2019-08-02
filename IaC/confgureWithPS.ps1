eb6ec0570036388e53241e14ec3ee17962f44989
abel <abel.wang@gmail.com>
2019-08-02 14:34:18 -0700

    removed everything

--- IaC/confgureWithPS.ps1
+++ IaC/confgureWithPS.ps1
@@ -1,11 +1,31 @@
-# setup backup for sql server
-#
-Write-Output "installing az.sql power shell..."
-Install-Module -Name Az.Sql -AllowClobber -Scope CurrentUser -Force
-Write-Output "done installing az.sql powershell"
+# # This is an IaC script to provision the web and database into azure
+# # for the ms-learn module for DB's
+# #
+# [CmdletBinding()]
+# param(
+#     [Parameter(Mandatory = $True)]
+#     [string]
+#     $resourceGroupName,
 
-Write-Output "creating backup plan..."
-Set-AzSqlInstanceDatabaseBackupShortTermRetentionPolicy -ResourceGroupName $resourceGroupName -InstanceName $servername -DatabaseName $dbName -RetentionDays 35
-Write-Output "done creating backup plan"
+#     [Parameter(Mandatory = $True)]
+#     [string]
+#     $servername,
+
+#     [Parameter(Mandatory = $True)]
+#     [string]
+#     $dbName
+# )
+
+
+
+# # setup backup for sql server
+# #
+# Write-Output "installing az.sql power shell..."
+# Install-Module -Name Az.Sql -AllowClobber -Scope CurrentUser -Force
+# Write-Output "done installing az.sql powershell"
+
+# Write-Output "creating backup plan..."
+# Set-AzSqlInstanceDatabaseBackupShortTermRetentionPolicy -ResourceGroupName $resourceGroupName -InstanceName $servername -DatabaseName $dbName -RetentionDays 35
+# Write-Output "done creating backup plan"
 
 
