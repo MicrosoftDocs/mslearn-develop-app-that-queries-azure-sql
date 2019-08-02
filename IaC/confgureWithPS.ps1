e2d2950c10b6c572deda89751952b91599f98700
abel <abel.wang@gmail.com>
2019-08-02 15:08:25 -0700

    removed everything

--- IaC/confgureWithPS.ps1
+++ IaC/confgureWithPS.ps1
@@ -1,33 +1,33 @@
-# This is an IaC script to provision the web and database into azure
-# for the ms-learn module for DB's
-#
-[CmdletBinding()]
-param(
-    [Parameter(Mandatory = $True)]
-    [string]
-    $resourceGroupName,
+# # This is an IaC script to provision the web and database into azure
+# # for the ms-learn module for DB's
+# #
+# [CmdletBinding()]
+# param(
+#     [Parameter(Mandatory = $True)]
+#     [string]
+#     $resourceGroupName,
 
-    [Parameter(Mandatory = $True)]
-    [string]
-    $servername,
+#     [Parameter(Mandatory = $True)]
+#     [string]
+#     $servername,
 
-    [Parameter(Mandatory = $True)]
-    [string]
-    $dbName
-)
+#     [Parameter(Mandatory = $True)]
+#     [string]
+#     $dbName
+# )
 
-# Uninstall AzureRM
-#
-Uninstall-AzureRm
+# # Uninstall AzureRM
+# #
+# Uninstall-AzureRm
 
-# setup backup for sql server
-#
-Write-Output "installing az.sql power shell..."
-Install-Module -Name Az.Sql -AllowClobber -Scope CurrentUser -Force
-Write-Output "done installing az.sql powershell"
+# # setup backup for sql server
+# #
+# Write-Output "installing az.sql power shell..."
+# Install-Module -Name Az.Sql -AllowClobber -Scope CurrentUser -Force
+# Write-Output "done installing az.sql powershell"
 
-Write-Output "creating backup plan..."
-Set-AzSqlInstanceDatabaseBackupShortTermRetentionPolicy -ResourceGroupName $resourceGroupName -InstanceName $servername -DatabaseName $dbName -RetentionDays 35
-Write-Output "done creating backup plan"
+# Write-Output "creating backup plan..."
+# Set-AzSqlInstanceDatabaseBackupShortTermRetentionPolicy -ResourceGroupName $resourceGroupName -InstanceName $servername -DatabaseName $dbName -RetentionDays 35
+# Write-Output "done creating backup plan"
 
 
