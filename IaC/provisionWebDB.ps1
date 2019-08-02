2846d255997a804a7d0bea4b7930d3c4d703f0d4
abel <abel.wang@gmail.com>
2019-08-02 14:04:14 -0700

    trying to force install

--- IaC/provisionWebDB.ps1
+++ IaC/provisionWebDB.ps1
@@ -311,7 +311,7 @@ Upload-DefaultData -dbServerName $servername -dbId $dbName -userId $adminLogin -
 
 # add command to set up 
 Write-Output "installing az.sql power shell..."
-Install-Module -Name Az.Sql -AllowClobber -Scope CurrentUser
+Install-Module -Name Az.Sql -AllowClobber -Scope CurrentUser -Force
 Write-Output "done installing az.sql powershell"
 
 Write-Output "creating backup plan..."
